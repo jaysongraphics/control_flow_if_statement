2575546ddcacee2496986b5b580c4516c73b0e02
Edward Arias <79659253+jaysongraphics@users.noreply.github.com>
2022-07-22 19:54:14 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+
+
+if height > 130:
+  print('Go head and enjoy!')
+else:
+  print('Your are too short, sorry.')
